5217ddc74e39b3db6a6f760202f8ccb95a39a44f
Stsiapan Zybin <stewarttankkinia@gmail.com>
2023-11-24 16:27:39 +0300

    "Add logger"

--- bot.py
+++ bot.py
@@ -12,112 +12,126 @@ from steam import SteamBot
 app_storage = {}
 login = argv[1]
 client = SteamBot(config.logInfoList[login])
-logging.basicConfig(level=logging.INFO, filename="logs/bot.log", filemode="w",
-                    format="%(asctime)s %(levelname)s %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
 
 #Checking in/outcoming trades
 #Approximate time 3s.
 async def check_trades():
-    print('check_trades')
-    logging.info(f'Check trades of {client.login} lunched')
+    logging.info(f'check_trades of {client.login} lunched')
     while True:
+        logging.info(f'Start checking trades of {client.login}')
         trades = await csgotm.get_trades(client)
         if trades['success']:
             await client.accept_trades(trades['trades'])
             client.steam_client.commit_confirmation_list()
+        logging.info(f'Trades of {client.login} was taken')
         await asyncio.sleep(30)
 
 
 #Getting steam balance and creating buy orders if balance > 20$
 #Approximately 110s
 async def check_steam_balance():
-    print('check_steam_balance')
+    logging.info(f'check_steam_balance of {client.login} lunched')
     while True:
+        logging.info(f'Start getting balance of {client.login}')
         listings = (await client.get_steam_listings())['buy_orders']
         await client.delete_buy_orders(listings.keys())
         balance = await client.get_balance()
         if balance > 20:
             await client.create_buy_orders(balance * 1000)
+        logging.info(f'Balance of {client.login} was got')
         await asyncio.sleep(43200)
 
 
 #Check tm balance and create cases buy order on tm
 #~170s.
 async def check_tm_balance() -> None:
-    print('check_tm_balance')
+    logging.info(f'check_tm_balance of {client.login} lunched')
     while True:
+        logging.info(f'Getting tm balance of {client.login}')
         balance = await csgotm.get_balance(client)
         if balance >= 5:
             await csgotm.buy_cases(client)
+        logging.info(f'Tm balance of {client.login} was got')
         await asyncio.sleep(3_600)
 
 
 #Find cases and sell them
 #Approximately 10s
 async def check_cases_in_steam_inventory():
-    print('check_cases_in_steam_inventory')
+    logging.info(f'check_cases_in_steam_inventory of {client.login} lunched')
     while True:
-        t = time.time()
+        logging.info(f'Start checking cases in inventory of {client.login}')
         await client.sell_cases_from_inventory()
-        print(time.time() - t)
+        logging.info(f'Cases in inventory of {client.login} was checked')
         await asyncio.sleep(3_600)
 
 
 #Create sell orders on tm
 #~15s
 async def check_items_to_sell_on_tm():
-    print('check_items_to_sell_on_tm')
+    logging.info(f'check_items_to_sell_on_tm of {client.login} lunched')
     while True:
+        logging.info(f'Start checking items of {client.login} to sell on tm')
         await csgotm.create_new_listings_on_tm(client)
+        logging.info(f'Finish checking items of {client.login} to sell on tm')
         await asyncio.sleep(44_000)
 
 
 #Check if sell price is close to current
 #~20s
 async def check_steam_listings():
-    print('check_steam_listings')
-    cmp_date: datetime.date
+    logging.info(f'check_steam_listings of {client.login} lunched')
     while True:
+        logging.info(f'Start checking steam listings of {client.login}')
+        cmp_date: datetime.date
         listings = (await client.get_steam_listings())['sell_listings']
         if len(listings.keys()):
             await client.change_case_listings(listings=listings)
+        logging.info(f'Finish checking steam listings of {client.login}')
         await asyncio.sleep(86_400)
 
 
 #Check selling items
 #~10
 async def check_tm_listings():
+    logging.info(f'check_tm_listings of {client.login} lunched')
     while True:
+        logging.info(f'Start checking tm listings of {client.login}')
         await csgotm.get_history(client)
-        t = time.time()
         await csgotm.check_listings(client)
-        print(time.time() - t)
+        logging.info(f'Finish checking tm listings of {client.login}')
         await asyncio.sleep(14_400)
 
 
 #Get latest deals in steam
 #~5
 async def check_steam_deals():
-    print('check_steam_deals')
+    logging.info(f'check_steam_deals of {client.login} lunched')
     while True:
+        logging.info(f'Start checking steam deals of {client.login}')
         await client.check_deals()
+        logging.info(f'Finish checking steam deals of {client.login}')
         await asyncio.sleep(3_600)
 
 
 #Get latest deals in tm
 #~5
 async def check_tm_deals():
-    print('check_tm_deals')
+    logging.info(f'check_tm_deals of {client.login} lunched')
     while True:
+        logging.info(f'Start checking tm deals of {client.login}')
         await csgotm.check_deals(client)
+        logging.info(f'Finish checking tm deals of {client.login}')
         await asyncio.sleep(3_600)
 
 
 async def ping_pong() -> None:
-    print('ping_pong')
+    logging.info(f'ping_pong of {client.login} lunched')
     url = f'https://market.csgo.com/api/v2/ping?key={client.tmApiKey}'
     while True:
+        logging.info(f'ping of {client.login}')
         while True:
             async with app_storage['session'].get(url=url) as response:
                 if response.status == 200:
@@ -125,6 +139,7 @@ async def ping_pong() -> None:
                     if answer["success"]:
                         break
                 await asyncio.sleep(3)
+        logging.info(f'pong of {client.login}')
         await asyncio.sleep(150)
 
 
@@ -142,7 +157,6 @@ async def main():
                  asyncio.create_task(check_tm_deals()),
                  asyncio.create_task(check_items_to_sell_on_tm()),
                  asyncio.create_task(check_cases_in_steam_inventory())]
-        # tasks = [asyncio.create_task(check_tm_deals())]
         await asyncio.gather(*tasks)
 
 
